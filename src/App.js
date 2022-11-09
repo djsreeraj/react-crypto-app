1ff5a2497ecf52cf31a54f97081ff7d1a37cee20
djsreeraj <sreeraj011m@gmail.com>
2022-11-09 06:17:23 +0530

    axios data fetch from api added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    axios.get('https://api.coinstats.app/public/v1/coins?skip=0').then(
+    (response) => console.log(response)
+    )
+  }, [])
+  
   return (
     <div className="App">
       <div className='cryptoHeader'>
