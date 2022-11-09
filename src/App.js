f8a70384cec407060bb3b93736b73c11c01ded60
djsreeraj <sreeraj011m@gmail.com>
2022-11-09 06:25:50 +0530

    displayed list of coins

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
 import './App.css';
 
 function App() {
+  const [listOfCoins, setListOfCoins] = useState([]);
   useEffect(() => {
     axios.get('https://api.coinstats.app/public/v1/coins?skip=0').then(
-    (response) => console.log(response)
+    (response) => setListOfCoins(response.data.coins)
     )
   }, [])
   
@@ -15,7 +16,7 @@ function App() {
 
       </div>
       <div className='cryptoDisplay'>
-
+          {listOfCoins.map( item => <h1>{item.name}</h1>)}
       </div>
 
     </div>
